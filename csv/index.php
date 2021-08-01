48046ce37e83308c189f0f13f49ddd8b4e52f757
kavithasarah <47032140+kavithasarah@users.noreply.github.com>
2021-08-01 13:49:48 +0530

    Add files via upload

---
+++ csv/index.php
@@ -0,0 +1,219 @@
+<?php
+use Migrate\DataSource;
+
+require_once 'DataSource.php';
+$db = new DataSource();
+$conn = $db->getConnection();
+
+if (isset($_POST["import"])) {
+    
+    $fileName = $_FILES["file"]["tmp_name"];
+    
+    if ($_FILES["file"]["size"] > 0) {
+        
+        $file = fopen($fileName, "r");
+        
+        while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+            
+            $eId = "";
+            if (isset($column[0])) {
+                $eId = mysqli_real_escape_string($conn, $column[0]);
+            }
+            $eName = "";
+            if (isset($column[1])) {
+                $eName = mysqli_real_escape_string($conn, $column[1]);
+            }
+            $address = "";
+            if (isset($column[2])) {
+                $address = mysqli_real_escape_string($conn, $column[2]);
+            }
+            $qualification = "";
+            if (isset($column[3])) {
+                $qualification = mysqli_real_escape_string($conn, $column[3]);
+            }
+            $passedout = "";
+            if (isset($column[4])) {
+                $passedout = mysqli_real_escape_string($conn, $column[4]);
+            }
+            
+            $sqlInsert = "INSERT into employees (eid,ename,address,qualification,passedout)
+                   values (?,?,?,?,?)";
+            $paramType = "issss";
+            $paramArray = array(
+                $eId,
+                $eName,
+                $address,
+                $qualification,
+                $passedout
+            );
+            $insertId = $db->insert($sqlInsert, $paramType, $paramArray);
+            
+            if (! empty($insertId)) {
+                $type = "success";
+                $message = "Your CSV file Migrated successfully";
+            } else {
+                $type = "error";
+                $message = "Problem in Migrating CSV Data";
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<script src="jquery-3.2.1.min.js"></script>
+
+<style>
+body {
+    font-family: Arial;
+    width: 550px;
+}
+
+.outer-scontainer {
+    background: red;
+    border: #e0dfdf 1px solid;
+    padding: 20px;
+    border-radius: 2px;
+}
+
+.input-row {
+    margin-top: 0px;
+    margin-bottom: 20px;
+}
+
+.btn-submit {
+    background: blue;
+    border: #1d1d1d 1px solid;
+    color: #f0f0f0;
+    font-size: 0.9em;
+    width: 100px;
+    border-radius: 2px;
+    cursor: pointer;
+}
+
+.outer-scontainer table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+.outer-scontainer th {
+    border: 1px solid #dddddd;
+    padding: 8px;
+    text-align: left;
+}
+
+.outer-scontainer td {
+    border: 1px solid #dddddd;
+    padding: 8px;
+    text-align: left;
+}
+
+#response {
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 2px;
+    display: none;
+}
+
+.success {
+    background: blue;
+    border: #bbe2cd 1px solid;
+}
+
+.error {
+    background: blue;
+    border: #f3c6c7 1px solid;
+}
+
+div#response.display-block {
+    display: block;
+}
+</style>
+<script type="text/javascript">
+$(document).ready(function() {
+    $("#frmCSVImport").on("submit", function () {
+
+	    $("#response").attr("class", "");
+        $("#response").html("");
+        var fileType = ".csv";
+        var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + fileType + ")$");
+        if (!regex.test($("#file").val().toLowerCase())) {
+        	    $("#response").addClass("error");
+        	    $("#response").addClass("display-block");
+            $("#response").html("Invalid File. Upload : <b>" + fileType + "</b> Files.");
+            return false;
+        }
+        return true;
+    });
+});
+</script>
+</head>
+
+<body>
+    <h2>Project : Migrating CSV Files</h2>
+
+    <div id="response"
+        class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>">
+        <?php if(!empty($message)) { echo $message; } ?>
+        </div>
+    <div class="outer-scontainer">
+        <div class="row">
+
+            <form class="form-horizontal" action="" method="post"
+                name="frmCSVImport" id="frmCSVImport"
+                enctype="multipart/form-data">
+                <div class="input-row">
+                    <label class="col-md-4 control-label">Choose CSV
+                        File</label> <input type="file" name="file"
+                        id="file" accept=".csv">
+                    <button type="submit" id="submit" name="import"
+                        class="btn-submit">Migrate</button>
+                    <br />
+
+                </div>
+
+            </form>
+
+        </div>
+               <?php
+            $sqlSelect = "SELECT * FROM employees";
+            $result = $db->select($sqlSelect);
+            if (! empty($result)) {
+                ?>
+            <table id='userTable'>
+            <thead>
+                <tr>
+                    <th>E_Id</th>
+                    <th>Employee Name</th>
+                    <th>Address</th>
+                    <th>Qualification</th>
+                    <th>Passedout</th>
+
+                </tr>
+            </thead>
+<?php
+                
+                foreach ($result as $row) {
+                    ?>
+                    
+                <tbody>
+                <tr>
+                    <td><?php  echo $row['eid']; ?></td>
+                    <td><?php  echo $row['ename']; ?></td>
+                    <td><?php  echo $row['address']; ?></td>
+                    <td><?php  echo $row['qualification']; ?></td>
+                    <td><?php  echo $row['passedout']; ?></td>
+                </tr>
+                    <?php
+                }
+                ?>
+                </tbody>
+        </table>
+        <?php } ?>
+    </div>
+
+</body>
+
+</html>
